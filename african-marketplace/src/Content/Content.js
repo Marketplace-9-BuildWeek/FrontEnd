006c99f00188fe5f0d16f78e5d6e6273d08d2174
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-12-20 16:53:57 -0500

    Structured components and set up the routes

---
+++ african-marketplace/src/Content/Content.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <div className="box-container">
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+    </div>
+  );
+};
+
+export default Content;
