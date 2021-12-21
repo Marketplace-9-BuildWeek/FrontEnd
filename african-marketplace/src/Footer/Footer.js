371d0a2c0d56b0fa46c02f760f66092c08fbb031
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-12-21 02:05:45 -0500

    Add social icons to footer

--- african-marketplace/src/Footer/Footer.js
+++ african-marketplace/src/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Footer.css";
+import { Twitter, Instagram, Facebook, Dribbble } from "react-feather";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>Footer Here</h1>
+      <Twitter size={30} />
+      <Instagram size={30} />
+      <Facebook size={30} />
+      <Dribbble size={30} />
     </div>
   );
 };
