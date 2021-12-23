779eb30581102e94b14a06a461c862ed430d6373
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-12-23 02:18:52 -0500

    Added Item create page

--- african-marketplace/src/Content/Content.js
+++ african-marketplace/src/Content/Content.js
@@ -1,44 +1,48 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import axios from "axios";
 import axiosWithAuth from "../utils/axiosWithAuth";
-
 import "./Content.css";
 import Item from "../Item/Item";
 
-const Content = (props) => {
+const Content = () => {
   // const { items } = props;
   const [items, setItems] = useState([]);
+  const [returnToken] = useState(localStorage.getItem("token"));
+  // const returnToken = localStorage.getItem("token");
 
   useEffect(() => {
     axiosWithAuth()
       .get("/listings")
       .then((res) => {
         console.log(res);
+        setItems(res.data);
       });
-    setItems(props.items);
-  }, []);
+  }, [returnToken]);
 
-  return (
-    <div className="wrapper">
-      {items.map((item) => {
-        return (
-          <div key={item.listing_id} className="box">
-            <Item
-              {...item}
+  if (returnToken) {
+    return (
+      <div className="wrapper">
+        {items.map((item) => {
+          return (
+            <div key={item.listing_id} className="box">
+              <Item
+                {...item}
 
-              // listing_id={item.listing_id}
-              // name={item.name}
-              // category={item.category}
-              // location_id={item.location_id}
-              // price={item.price}
-              // description={item.description}
-            />
-          </div>
-        );
-      })}
-    </div>
-  );
+                // listing_id={item.listing_id}
+                // name={item.name}
+                // category={item.category}
+                // location_id={item.location_id}
+                // price={item.price}
+                // description={item.description}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return "Please log in ";
+  }
 };
 
 export default Content;
---
+++ african-marketplace/src/validation/FormSchema.js
@@ -0,0 +1,32 @@
+import * as yup from "yup";
+
+// object must have:
+// -name: required, string
+// -category: required, string, max 50 chars
+// -description: required, string, max 150 chars
+// -price: required, non-zero, integer
+// -location_id: required, non-zero, integer (points to a preset database table, value must be 1 through 6)
+
+const FormSchema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  category: yup
+    .string()
+    .required("Category is required")
+    .max(50, "category should not be more than 50 characters"),
+  description: yup
+    .string()
+    .required("A description about the product is required")
+    .max(150, "Max 150 characters"),
+  price: yup
+    .number()
+    .required("Price is required")
+    .min(1, "Price must be greater than zero"),
+  location_id: yup
+    .number()
+    .nullable()
+    .required("Location is required")
+    .min(0, "Value must be 1 through 6")
+    .max(6, "Value must be 1 through 6"),
+});
+
+export default FormSchema;
